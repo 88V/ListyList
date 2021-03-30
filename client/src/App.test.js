5097eb0abef5ffbc89ae76acefdbe2e54759daae
John San Pietro <msanpietro@gmail.com>
2021-03-30 09:41:20 -0400

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import { shallow, mount } from 'enzyme';
+import toJson from "enzyme-to-json";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders the app', () => {
+//   const component = render(<App />);
+//   expect(component).toMatchSnapshot();
+// });
+
+describe("rendering components", () => {
+  it("renders App component without crashing", () => {
+    shallow(<App />);
+  });
+
 });
